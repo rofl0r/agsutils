a9c7b476f4c1aa4900ee559010f758daa9687a18
rofl0r <rofl0r@users.noreply.github.com>
2019-06-03 22:57:44 +0100

    agssemble: add possibility to use preprocessor

--- agssemble.c
+++ agssemble.c
@@ -7,20 +7,23 @@
 #include "version.h"
 #define ADS ":::AGSsemble " VERSION " by rofl0r:::"
 
-__attribute__((noreturn))
-void usage(char *argv0) {
-	dprintf(2, ADS "\nusage:\n%s file.s [file.o]\npass an ags assembly filename.\n"
+static int usage(char *argv0) {
+	dprintf(2, ADS "\nusage:\n%s [-E] file.s [file.o]\npass an ags assembly filename.\n"
+	"-E: invoke C preprocessor 'cpp' on the input file before assembling\n"
 	"if optional second filename is ommited, will write into file.o\n", argv0);
-	exit(1);
+	return 1;
 }
 
 int main(int argc, char** argv) {
-	if(argc < 2 || argc > 3) usage(argv[0]);
-	AS a_b, *a = &a_b;
-	char* file = argv[1];
-	AS_open(a, file);
-	char out [256], *outn;
-	if(argc == 2) {
+	int flags = 0, c;
+		while ((c = getopt(argc, argv, "E")) != EOF) switch(c) {
+		case 'E': flags |= 1; break;
+		default: return usage(argv[0]);
+	}
+	if(!argv[optind]) return usage(argv[0]);
+	char* file = argv[optind];
+	char out [256], *outn, tmp[256];
+	if(!argv[optind+1]) {
 		size_t l = strlen(file);
 		char *p;
 		snprintf(out, 256, "%s", file);
@@ -30,11 +33,29 @@ int main(int argc, char** argv) {
 		*(p++) = 'o';
 		*p = 0;
 		outn = out;
-	} else outn = argv[2];
+	} else outn = argv[optind+1];
+
+	if(flags & 1) {
+		snprintf(tmp, sizeof tmp, "%s.i", file);
+		dprintf(1, "preprocessing %s -> %s ...", file, tmp);
+		char cmd[1024];
+		snprintf(cmd, sizeof cmd, "cpp %s > %s", file, tmp);
+		int ret = system(cmd);
+		if(ret) {
+			dprintf(1, "FAIL\n");
+			return ret;
+		}
+		dprintf(1, "OK\n");
+		file = tmp;
+	}
+
+	AS a_b, *a = &a_b;
+	AS_open(a, file);
+
 	dprintf(1, "assembling %s -> %s ... ", file, outn);
 	int ret = AS_assemble(a, outn);
 	AS_close(a);
-	
+
 	if(!ret) dprintf(1, "FAIL\n");
 	else dprintf(1, "OK\n");
 	return !ret;
