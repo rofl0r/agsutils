81367a379b249734065dad400cecfdee6d548dee
rofl0r <rofl0r@users.noreply.github.com>
2019-12-05 01:02:18 +0000

    StringEscape.c: support full range of C escape characters
    `Blackwell Deception` demo uses a string with \f in it.
    
    - updated to latest version of gettext-tiny
    - ordered statements according to binary value
    - hex/oct and error detection are still missing
      (strings that require more than 4096 output chars are
       currently truncated)

--- StringEscape.c
+++ StringEscape.c
@@ -1,51 +1,53 @@
 #include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+
+//FIXME out gets silently truncated if outsize is too small
 
 size_t escape(char* in, char* out, size_t outsize) {
 	size_t l = 0;
 	while(*in && l + 3 < outsize) {
 		switch(*in) {
-			case '\n':
-				*out++ = '\\';
-				l++;
-				*out = 'n';
-				break;
-			case '\r':
-				*out++ = '\\';
-				l++;
-				*out = 'r';
-				break;
-			case '\t':
-				*out++ = '\\';
-				l++;
-				*out = 't';
-				break;
-			case '\\':
-				*out++ = '\\';
-				l++;
-				*out = '\\';
-				break;
-			case '"':
-				*out++ = '\\';
-				l++;
-				*out = '"';
-				break;
-			case '\v':
-				*out++ = '\\';
-				l++;
-				*out = '\v';
-				break;
-			case '\?':
-				*out++ = '\\';
-				l++;
-				*out = '\?';
-				break;
-			case '\f':
-				*out++ = '\\';
-				l++;
-				*out = '\f';
-				break;
-			default:
-				*out = *in;
+		case '\a': /* 0x07 */
+		case '\b': /* 0x08 */
+		case '\t': /* 0x09 */
+		case '\n': /* 0x0a */
+		case '\v': /* 0x0b */
+		case '\f': /* 0x0c */
+		case '\r': /* 0x0d */
+		case '\"': /* 0x22 */
+		case '\'': /* 0x27 */
+		case '\?': /* 0x3f */
+		case '\\': /* 0x5c */
+			*out++ = '\\';
+			l++;
+			switch(*in) {
+			case '\a': /* 0x07 */
+				*out = 'a'; break;
+			case '\b': /* 0x08 */
+				*out = 'b'; break;
+			case '\t': /* 0x09 */
+				*out = 't'; break;
+			case '\n': /* 0x0a */
+				*out = 'n'; break;
+			case '\v': /* 0x0b */
+				*out = 'v'; break;
+			case '\f': /* 0x0c */
+				*out = 'f'; break;
+			case '\r': /* 0x0d */
+				*out = 'r'; break;
+			case '\"': /* 0x22 */
+				*out = '\"'; break;
+			case '\'': /* 0x27 */
+				*out = '\''; break;
+			case '\?': /* 0x3f */
+				*out = '\?'; break;
+			case '\\': /* 0x5c */
+				*out = '\\'; break;
+			}
+			break;
+		default:
+			*out = *in;
 		}
 		in++;
 		out++;
@@ -54,43 +56,49 @@ size_t escape(char* in, char* out, size_t outsize) {
 	*out = 0;
 	return l;
 }
-#include <assert.h>
-#include <stdlib.h>
+
 size_t unescape(char* in, char *out, size_t outsize) {
 	size_t l = 0;
-	while(*in && l + 2 < outsize) {
+	while(*in && l + 1 < outsize) {
 		switch (*in) {
 			case '\\':
 				++in;
 				assert(*in);
 				switch(*in) {
-					case 'n':
-						*out='\n';
+					case 'a':
+						*out = '\a';
 						break;
-					case 'r':
-						*out='\r';
+					case 'b':
+						*out = '\b';
 						break;
 					case 't':
 						*out='\t';
 						break;
-					case '\\':
-						*out='\\';
-						break;
-					case '"':
-						*out='"';
+					case 'n':
+						*out='\n';
 						break;
 					case 'v':
 						*out='\v';
 						break;
-					case '\?':
-						*out = '\?';
-						break;
 					case 'f':
 						*out = '\f';
+						break;
+					case 'r':
+						*out='\r';
+						break;
+					case '"':
+						*out='"';
 						break;
 					case '\'':
 						*out = '\'';
 						break;
+					case '?':
+						*out = '\?';
+						break;
+					case '\\':
+						*out='\\';
+						break;
+					// FIXME add handling of hex and octal
 					default:
 						abort();
 				}
@@ -105,4 +113,3 @@ size_t unescape(char* in, char *out, size_t outsize) {
 	*out = 0;
 	return l;
 }
-
