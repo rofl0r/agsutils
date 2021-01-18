4a466aad1de401a21cb6e03f74bda5ec3e086760
rofl0r <rofl0r@users.noreply.github.com>
2021-01-18 23:38:17 +0000

    agscriptxtract: turns out that header-things are only exported in 2.7x
    AGS 2.62 exports different things, in particular:
    
        #define EGO 0
        #define STATUSLINE FindGUIID("STATUSLINE")
        #define ICONBAR FindGUIID("ICONBAR")
        #define INVENTORY FindGUIID("INVENTORY")
        #define VIEW1 1
        #define VIEW2 2
    
    the rest is imported from the equivalent of agsdefns.(a)sh - so the game
    specific header part is actually much slimmer than in 2.72.
    the only things exported are:
    
    - "Script name" of Character
    - the 3 gui names, as findguiid macro
    - the view names mapped to their numeric value.

--- agscriptxtract.c
+++ agscriptxtract.c
@@ -109,6 +109,7 @@ void dump_script(AF* f, ASI* s, char* fn, int flags) {
 void dump_header(ADF *a, char *fn) {
 	unsigned i;
 	FILE *f = fopen(fn, "w");
+	fprintf(f, "#if SCRIPT_API < 300000 && SCRIPT_API > 262000\n");
 	if(ADF_get_cursorcount(a)) fprintf(f, "enum CursorMode {\n");
 	for(i=0; i<ADF_get_cursorcount(a); ++i) {
 		if(i > 0) fprintf(f, ",\n");
@@ -121,6 +122,7 @@ void dump_header(ADF *a, char *fn) {
 	}
 	if(i) fprintf(f, "};\n");
 	fprintf(f, "import Character character[%zu];\n", ADF_get_charactercount(a));
+	fprintf(f, "#endif\n");
 	fclose(f);
 }
 
