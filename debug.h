7e1b821b123c7afdbefb4a03b91bdbc38e8c6907
rofl0r <rofl0r@users.noreply.github.com>
2022-01-04 22:19:46 +0000

    debug.h: fix some unportable code

--- debug.h
+++ debug.h
@@ -2,9 +2,9 @@
 #define DEBUG_H
 
 #define empty_body do {} while(0)
-#if defined(__x86_64) || defined(__x86_64__)
+#if defined(__x86_64) || defined(__x86_64__) || defined(_M_X64)
 #  define INTEL_CPU 64
-#elif defined(__x86) || defined(__x86__) || defined(__i386) || defined(__i386__)
+#elif defined(__x86) || defined(__x86__) || defined(__i386) || defined(__i386__) || defined(_M_IX86)
 #  define INTEL_CPU 32
 #endif
 
@@ -13,12 +13,24 @@
 #  define breakpoint() empty_body
 #else
 #  ifdef INTEL_CPU
+#   ifdef __GNUC__
 #    define breakpoint() __asm__("int3")
-#  else
+#   elif defined(__POCC__)
+#    define breakpoint() __asm { int3 }
+#   else
+#    define breakpoint() empty_body
+#   endif
+#  elif defined __unix__
 #    include <signal.h>
 #    include <unistd.h>
 #    define breakpoint() kill(getpid(), SIGTRAP)
+#  else
+#   define breakpoint() empty_body
 #  endif
+#endif
+
+#if !defined(__GNUC__) && (defined(__POCC__) || __STDC_VERSION__+0 >= 199901L)
+#define __FUNCTION__ __func__
 #endif
 
 //#define NO_ASSERT
