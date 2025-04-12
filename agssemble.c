8dce129c2aed143d35cd87c401397d01987ae3ab
rofl0r <rofl0r@users.noreply.github.com>
2025-04-12 09:52:56 +0000

    agssemble: make preprocessor support optional
    i'm probably the only person using that feature, but it
    prevents compilation with mingw because the preprocessor
    code used fmemopen() and similar stdio functions that
    are lacking in mingw.
    we'll wire up the makefile in the next step.

--- agssemble.c
+++ agssemble.c
@@ -1,36 +1,50 @@
 #define _GNU_SOURCE
+#ifdef __POCC__
 #define __STDC_WANT_LIB_EXT2__ 1
+#endif
 #include <stdio.h>
 #include "Assembler.h"
 #include "DataFile.h"
-#include "preproc.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include "version.h"
 #define ADS ":::AGSsemble " VERSION " by rofl0r:::"
 
+#ifndef DISABLE_CPP
+#include "preproc.h"
+#endif
+
 static int usage(char *argv0) {
 	fprintf(stderr, ADS "\nusage:\n%s [-E] [-i file.i] [-I includedir] [-D preproc define] file.s [file.o]\n"
 	"pass an ags assembly filename.\n"
+#ifndef DISABLE_CPP
 	"-E: invoke built-in C preprocessor 'tinycpp' on the input file before assembling\n"
 	"-I includedir - add include dir for CPP\n"
 	"-D define - add define for CPP\n"
 	"-i file save preprocessor output to file\n"
+#else
+	"preprocessor disabled in this build, so option -E,-I,-D,-i are ignored\n"
+#endif
 	"if optional second filename is ommited, will write into file.o\n", argv0);
 	return 1;
 }
 
+#ifndef DISABLE_CPP
 static FILE *freopen_r(FILE *f, char **buf, size_t *size) {
 	fflush(f);
 	fclose(f);
 	return fmemopen(*buf, *size, "r");
 }
+#endif
 
 int main(int argc, char** argv) {
+#ifndef DISABLE_CPP
 	struct cpp* cpp = cpp_new();
+#endif
 	char *tmp, *cppoutfn = 0;
 	int flags = 0, c;
 		while ((c = getopt(argc, argv, "Ei:I:D:")) != EOF) switch(c) {
+#ifndef DISABLE_CPP
 		case 'E': flags |= 1; break;
 		case 'i': cppoutfn = optarg; break;
 		case 'I': cpp_add_includedir(cpp, optarg); break;
@@ -38,6 +52,7 @@ int main(int argc, char** argv) {
 			if((tmp = strchr(optarg, '='))) *tmp = ' ';
 			cpp_add_define(cpp, optarg);
 			break;
+#endif
 		default: return usage(argv[0]);
 	}
 	if(!argv[optind]) return usage(argv[0]);
@@ -65,6 +80,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+#ifndef DISABLE_CPP
 	if(flags & 1) {
 		struct FILE_container {
 			FILE *f;
@@ -88,6 +104,7 @@ int main(int argc, char** argv) {
 		}
 	}
 	// cpp_free(cpp); // FIXME this crashes on windows, find out why
+#endif
 
 	AS a_b, *a = &a_b;
 	AS_open_stream(a, in);
